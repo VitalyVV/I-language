995fc3f07b86c767c342e39a1f8b252d79a00c36
asalikhzyanov <a.salikhzyanov@innopolis.ru>
2018-12-05 00:21:14 +0300

    Minor fixes in the Structure. Added Code Generation library.

--- src/Generator/Entities/Simple.java
+++ src/Generator/Entities/Simple.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
 
 public class Simple {
     private Factor main;
-    private String op;
+    private ArrayList<String> ops;
     private ArrayList<Factor> factors;
 
-    public Simple(Factor main, String op, ArrayList<Factor> factors) {
+    public Simple(Factor main, ArrayList<String> ops, ArrayList<Factor> factors) {
         this.main = main;
-        this.op = op;
+        this.ops = ops;
         this.factors = factors;
     }
 
-    public String getOp() {
-        return op;
+    public ArrayList<String> getOps() {
+        return ops;
     }
 
     public Factor getMain() {
