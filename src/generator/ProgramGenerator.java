89628d4982ccb45fa0f1190fc87a00535f0d8631
Eduard Zalyaev <edzaljaev@gmail.com>
2018-12-06 19:06:28 +0300

    Program generator draft

--- src/generator/ProgramGenerator.java
+++ src/generator/ProgramGenerator.java
@@ -1,93 +1,41 @@
-package helpers;
-import Syntax.WrongSyntaxException;
-import com.sun.corba.se.impl.io.TypeMismatchException;
-import jdk.nashorn.internal.runtime.regexp.joni.exception.SyntaxException;
+package generator;
 
 import java.util.*;
 
 //I think that functions for processing
 
-public class ProgramNode extends Node {
+public class ProgramNode extends Generator {
 
-    public String getMethod(){
-        return "Root";
-    }
+    ArrayList<HashMap<String, Object>> elements;
 
-    public ProgramNode(String name, ArrayList<HashMap<String, Object>> children){
+    public ProgramNode(String name, ArrayList<HashMap<String, Object>> elements){
         super(name);
-        this.children = children;
-    }
-
-    public LinkedHashMap<String, Symbol> getSymbols(){
-        return symbolsDeclarations;
+        this.elements = elements;
     }
 
     //Start method to go down the tree and get the types of objects
-    public void createSymbols() throws Exception{
-        for (HashMap<String, Object> elem: children){
+    public String generateRoot() throws Exception{
+        for (HashMap<String, Object> elem: elements){
 
             HashMap<String, Object> rootUnit = ((HashMap<String, Object>) elem.get("Content"));
             String unit = (String) rootUnit.get("statement");
-            if (unit.equals("var")) {//variable declaration type extraction
-                if (symbolsDeclarations.containsKey(rootUnit.get("name"))) throw new SyntaxException("Variable has already been declared: "+ rootUnit.get("name"));
-                if (typeMappings.containsKey(rootUnit.get("name"))) throw new SyntaxException("Type has already been declared: "+ rootUnit.get("name") +" can't create a variable with such name.");
+            if (unit.equals("var")) {// Variable declaration type extraction
+                String type = getType(rootUnit.get("type")); // Get type template or create object with template
+                String name = (String) rootUnit.get("name");
+                String expression = generateExpression(rootUnit.get("value")); //  equivalent to calculate expression result
 
-                if (((String) rootUnit.get("hastype")).equals("true")) {
-                    //if type is already declared via "var a: integer"
-                    Symbol s = null;
-                    String varType = getType(rootUnit.get("type"));
-                    if (varType.equals("record") || varType.equals("array"))
-                    {
-                        s = new Symbol(varType, (String) rootUnit.get("name"), rootUnit);
-                    } else if (rootUnit.containsKey("value")) { //Vars that have type may have "is" expressions too
-                        //Inference expression type
-                        String expressionResult = calculateExpressionResult(rootUnit.get("value"));
-                        if (varType.equals("boolean") &&  expressionResult.equals("integer") && !isIntBooleanable((HashMap<String, Object>) rootUnit.get("value"))) throw new WrongSyntaxException("Can't assign non 1 or 0 int to boolean");
-                        //If expression type is the same as declared type
-                        if (expressionResult.equals(varType) || (varType.equals("boolean") &&  expressionResult.equals("integer")))
-                        {
-                            s = new Symbol(varType, (String) rootUnit.get("name"), rootUnit);
-                        } else throw new TypeMismatchException("\nWrong expression result type for: " + rootUnit.get("name") +
-                                ".\nExpected: "+ varType+". Got: " + expressionResult);
+                //Add to body
 
-                    } else s = new Symbol(varType, (String) rootUnit.get("name"), rootUnit);
+            } else if(unit.equals("declaration")){
+                RoutineGenerator rg = new RoutineGenrator(unit.get(value));
+                String routineBody = rg.generateBody(); // equivalent to parseBody()
 
-                    symbolsDeclarations.put((String) rootUnit.get("name"), s);
-                } else {
-                    if (symbolsDeclarations.containsKey(rootUnit.get("name"))) throw new SyntaxException("Variable has already been declared: "+ rootUnit.get("name"));
-                    //if type is declared via expression calculation like "var a: integer is (5+5)*10"
-                    String valueType = calculateExpressionResult(rootUnit.get("expression"));
-                    Symbol s = new Symbol(valueType, (String) rootUnit.get("name"), rootUnit);
-                    symbolsDeclarations.put((String) rootUnit.get("name"), s);
-                }
-            } else if(unit.equals("declaration")){//if we have routine declaration "routine a() : integer"
-                if (namesRoutines.contains(rootUnit.get("name"))) throw new SyntaxException("Routine has already been declared: "+ rootUnit.get("name"));
-                RoutineNode routine = new RoutineNode((String) rootUnit.get("name"), rootUnit);
-                //Routines can't read declarations ahead of them
-                routine.setSymbols(symbolsDeclarations);
-                routine.setTypes(typeMappings);
-                routine.createTable();
-                //we add it separate list of routines
-                routines.add(routine);
-                //we add name of routine to find previously declared ones + to check if we actually have declared it
-                namesRoutines.add((String) rootUnit.get("name"));
-                symbolsDeclarations.put((String) rootUnit.get("name"),
-                        new Symbol("routine", (String) rootUnit.get("name"), routine));
+                //add to body
 
-                routineEndTypes.put((String) rootUnit.get("name"), getLastKnownType());
             }else if(unit.equals("type")){//if we have type declaration
-                if (symbolsDeclarations.containsKey(rootUnit.get("name"))) throw new SyntaxException("Variable has already been declared: "+ rootUnit.get("name")+" can't call a new type with such name");
-                if (typeMappings.containsKey(rootUnit.get("name"))) throw new SyntaxException("Type has already been declared: "+ rootUnit.get("name"));
-                //Add type mapping to the list
-                typeMappings.put((String)rootUnit.get("name"),getType(rootUnit.get("type")));
-                if (getType(rootUnit.get("type")).equals("record") || getType(rootUnit.get("type")).equals("array"))
-                {
-                    memberMappings.put((String) rootUnit.get("name"), rootUnit.get("type"));
-                }
-            } else throw new Exception("Invalid syntax. Routine or declaration expected");
+                //Generate object generation template
+            }
         }
-
-        entries = symbolsDeclarations.entrySet().iterator();
 
     }
 
